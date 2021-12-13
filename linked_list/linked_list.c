6b4ebcb59c9b9a7af4ae9151c4ee3b4acf740218
Abe <abe@habet.dev>
2021-12-13 15:07:51 -0500

    delete_from_list

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -12,6 +12,7 @@ struct node *first = NULL;
 struct node *add_to_list(struct node *list, int n);
 struct node *read_numbers(void);
 struct node *search_list(struct node *list, int n);
+struct node *delete_from_list(struct node *list, int n);
 
 int main(void)
 {
@@ -53,5 +54,24 @@ struct node *search_list(struct node *list, int n)
 {
     while (list != NULL&& list->value != n)
         list = list->next;
+    return list;
+}
+
+struct node *delete_from_list(struct node *list, int n)
+{
+    struct node *cur, *prev;
+
+    for (cur = list, prev = NULL;
+         cur != NULL && cur->value != n;
+         prev = cur, cur = cur->next)
+        ;
+
+    if (cur == NULL)
+        return list;            // n was not found
+    if (prev == NULL)
+        list = list->next;      // n is in the first node
+    else
+        prev->next = cur->next; // n is in some other node
+    free(cur);
     return list;
 }
