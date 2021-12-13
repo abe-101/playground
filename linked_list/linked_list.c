d361fd83a7303ca62b62752beda454ad62b034f1
Abe <abe@habet.dev>
2021-12-13 14:42:01 -0500

    read_numbers: promts user to enter number and adds to linked list

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -10,7 +10,7 @@ struct node *first = NULL;
 
 // prototypes
 struct node *add_to_list(struct node *list, int n);
-
+struct node *read_numbers(void);
 
 int main(void)
 {
@@ -34,3 +34,16 @@ struct node *add_to_list(struct node *list, int n)
     return new_node;
 }
 
+struct node *read_numbers(void)
+{
+    struct node *first = NULL;
+    int n;
+
+    printf("Enter a series of integers (0 to terminate): ");
+    for (;;) {
+        scanf("%d", &n);
+        if (n == 0)
+            return first;
+        first = add_to_list(first, n);
+    }
+}
